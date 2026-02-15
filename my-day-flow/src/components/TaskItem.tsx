53bf8d70ea108dcaeaf1dd967a4c90a24090e76b
Om Kumar <omkumar947062@gmail.com>
2026-02-15 21:03:36 +0530

    timer updated to 12 hrs 2

--- my-day-flow/src/components/TaskItem.tsx
+++ my-day-flow/src/components/TaskItem.tsx
@@ -38,6 +38,7 @@ const statusConfig = {
     icon: Check,
     className: 'bg-success/15 text-success',
   },
+  
 };
 
 export const TaskItem = ({ task, onStatusChange, onDelete, onEdit }: TaskItemProps) => {
